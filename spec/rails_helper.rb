846af74c9b5561f7e94d3cf51fb4dd0384b504bd
JoaquinSeita <joaquin.seita@widergy.com>
2021-11-11 10:09:07 -0300

    rails helper fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,10 +27,3 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
-
-VCR.configure do |config|
-  config.cassette_library_dir = "#{::Rails.root}/spec/cassettes"
-  config.hook_into :webmock
-  config.ignore_localhost = true
-  config.configure_rspec_metadata!
-end
